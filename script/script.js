b656a1658ec5cf0606a5e51d6259a64f8c90a0de
Руньковский Константин <raitok@mail.ru>
2021-05-29 16:01:27 +0300

    feat(script.js): add lesson 2

--- script/script.js
+++ script/script.js
@@ -5,5 +5,23 @@ let deposit;
 let mission;
 let period;
 
-alert ('Hello world');
-console.log('Привет мир');
+money = 100;
+income = 'Фриланс';
+addExpenses = 'Интернет, Такси, Коммуналка';
+deposit = true;
+mission = 1000;
+period = 10;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
