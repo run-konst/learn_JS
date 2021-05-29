fbf965682c6c4bbca9565517df9a1c834bccc761
Руньковский Константин <raitok@mail.ru>
2021-05-29 17:57:21 +0300

    refactor(script.js): правки

--- script/script.js
+++ script/script.js
@@ -1,18 +1,13 @@
 'use strict';
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-
-money = 100;
-income = 'Фриланс';
-addExpenses = 'Интернет, Такси, Коммуналка';
-deposit = true;
-mission = 1000;
-period = 10;
+const
+money = 100,
+income = 'Фриланс',
+addExpenses = 'Интернет, Такси, Коммуналка',
+deposit = true,
+mission = 1000,
+period = 10,
+budgetDay = money / 30;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -20,10 +15,5 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' долларов');
-
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(', ');
-console.log(addExpenses);
-
-let budgetDay = money / 30;
+console.log(addExpenses.toLowerCase().split(', '));
 console.log(budgetDay);
