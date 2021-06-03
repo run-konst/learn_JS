9b3a73532895c35260511886932a65a576c1cf06
Руньковский Константин <raitok@mail.ru>
2021-06-03 18:27:26 +0300

    refactor(script.js): правки

--- script/script.js
+++ script/script.js
@@ -4,15 +4,14 @@ const isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let one = function () {
-    let gameNum = 42;
-    return function gameStart () {
-        let playerNum = prompt('Угадай число от 1 до 100');
+const one = function () {
+    const gameNum = 42;
+    const gameStart = function () {
+        let playerNum = +prompt('Угадай число от 1 до 100');
         while (!isNumber(playerNum) || playerNum > 100 || playerNum < 1) {
             alert('Введи число от 1 до 100!');
-            playerNum = prompt('Угадай число от 1 до 100');
+            playerNum = +prompt('Угадай число от 1 до 100');
         }
-        playerNum = Number(playerNum);
         if (playerNum === gameNum) {
             alert('Поздравляю, Вы угадали!!!');
         } else if (playerNum > gameNum) {
@@ -23,7 +22,7 @@ let one = function () {
             gameStart();  
         }
     };
+    gameStart();
 };
 
-const two = one();
-two();
+one();
