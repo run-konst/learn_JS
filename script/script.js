03056692ece205fe16aef90577169d1114cfabf7
Руньковский Константин <raitok@mail.ru>
2021-06-17 18:08:35 +0300

    feat(script.js) add zero

--- script/script.js
+++ script/script.js
@@ -1,15 +1,22 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
 
     'use strict';
 
     let deadline = ('18 june 2021');
-    
+
     const
         timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds'),
         timerDate = document.querySelector('#timer-date'),
         timerBtn = document.querySelector('#timer-btn'),
+        addZero = (str) => {
+            if (String(str).length === 1) {
+                return '0' + str;
+            } else {
+                return str;
+            }
+        },
         setTimer = (deadline) => {
             const
                 dateNow = new Date().getTime(),
@@ -18,16 +25,16 @@ window.addEventListener('DOMContentLoaded', function () {
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 3600);
-            return {seconds, minutes, hours, timeRemaining};
-    
+            return { seconds, minutes, hours, timeRemaining };
+
         },
         updateTimer = () => {
             const timer = setTimer(deadline);
 
-            if (timer.timeRemaining > 0 ) {
-                timerHours.textContent = timer.hours;
-                timerMinutes.textContent = timer.minutes;
-                timerSeconds.textContent = timer.seconds;             
+            if (timer.timeRemaining > 0) {
+                timerHours.textContent = addZero(timer.hours);
+                timerMinutes.textContent = addZero(timer.minutes);
+                timerSeconds.textContent = addZero(timer.seconds);
             } else {
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
@@ -35,9 +42,9 @@ window.addEventListener('DOMContentLoaded', function () {
                 clearInterval(int);
             }
         };
-    
+
     timerBtn.addEventListener('click', () => {
-        deadline = new Date(timerDate.value);  
+        deadline = new Date(timerDate.value);
     });
 
     updateTimer();
