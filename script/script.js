e6779a3092c827ba40ebaef3d1a940a9a0788acf
Руньковский Константин <raitok@mail.ru>
2021-06-17 23:58:25 +0300

    feat(script.js) menu and popup

--- script/script.js
+++ script/script.js
@@ -2,22 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
     'use strict';
 
-    let deadline = ('18 june 2021');
+    // Timer
 
     const
         timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds'),
-        timerDate = document.querySelector('#timer-date'),
-        timerBtn = document.querySelector('#timer-btn'),
-        addZero = (str) => {
-            if (String(str).length === 1) {
-                return '0' + str;
-            } else {
-                return str;
-            }
-        },
-        setTimer = (deadline) => {
+        addZero = str => (str < 10 ? `0${str}` : str),
+        setTimer = deadline => {
             const
                 dateNow = new Date().getTime(),
                 dateStop = new Date(deadline).getTime(),
@@ -29,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         },
         updateTimer = () => {
-            const timer = setTimer(deadline);
+            const timer = setTimer('18 june 2021');
 
             if (timer.timeRemaining > 0) {
                 timerHours.textContent = addZero(timer.hours);
@@ -43,11 +35,46 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         };
 
-    timerBtn.addEventListener('click', () => {
-        deadline = new Date(timerDate.value);
-    });
-
     updateTimer();
     const int = setInterval(updateTimer, 1000);
 
+    // Menu
+
+    const
+        menu = document.querySelector('menu'),
+        menuBtn = document.querySelector('.menu'),
+        menuClose = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('li'),
+        toggleMenu = () => {
+            if (menu.style.transform === 'translate(-100%)' || !menu.style.transform) {
+                menu.style.transform = 'translate(0)';
+            } else {
+                menu.style.transform = 'translate(-100%)';
+            }
+        };
+
+    menuBtn.addEventListener('click', toggleMenu);
+    menuClose.addEventListener('click', toggleMenu);
+    menuItems.forEach(item => item.addEventListener('click', toggleMenu));
+
+    // Popup
+
+    const
+        popup = document.querySelector('.popup'),
+        popupContent = document.querySelector('.popup-content'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close'),
+        showPopup = () => {
+            popupContent.style.top = (parseInt(popupContent.style.top) - 1 + '%');
+        };
+
+    popupBtn.forEach(item => item.addEventListener('click', () => {
+        popup.style.display = 'block';
+        popupContent.style.top = '100%';
+        const animate = setInterval(showPopup, 10);
+        setTimeout(() => clearInterval(animate), 900);
+    }));
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
 });
